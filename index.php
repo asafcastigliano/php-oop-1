07c283f48d49c8e48568bb44b3031486ab03c3a6
asafcastigliano <asafcastigliano@gmail.com>
2024-01-30 15:51:22 +0100

    punto 1.2

--- index.php
+++ index.php
@@ -5,6 +5,13 @@ class Movie {
     public $genre;
     public $year;
     public $director;
+
+    public function __construct($title, $genre, $year, $director) {
+        $this->title = $title;
+        $this->genre = $genre;
+        $this->year = $year;
+        $this->director = $director;
+    }
 }
 
 ?>
